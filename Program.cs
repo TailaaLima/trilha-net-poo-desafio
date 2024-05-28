27d291a7153fb43f61f16d027d13c23b7c8a6b84
Taila Lima <tailalima.ds@gmail.com>
2024-05-28 20:58:39 -0300

    feat: Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone celularNokia = new Nokia(numero: "948072990", modelo: " Nokia C30", imei: "359517095710067", memoria: 32 );
+celularNokia.InstalarAplicativo("WhatsApp");
+celularNokia.Ligar();
+
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone celularIphone = new Iphone(numero: "958723695", modelo: "Iphone 13", imei: "359517099609406", memoria: 64);
+celularIphone.InstalarAplicativo("Instagram");
+celularIphone.ReceberLigacao();
